94a263b1feb65fe147ec7ab577b8b2d055851a40
Deeya <guptadeeya12@gmail.com>
2023-05-13 10:55:55 +0530

    Play song part-1

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
 console.log("Welcome to spotify");
+
+// Initialize the variables
+
+let songIndex=0;
+let audioElement = new Audio("1.mp3")
+let masterPlay = document.getElementById("masterPlay");
+let myProgressBar = document.getElementById("myProgressBar");
+let gif = document.getElementById("gif");
+
+let songs = [
+    {
+        songName: "Salam-e-Ishq", filePath: "song/1.mp3", coverPath: 'covers/1.jpg',
+        songName: "Salam-e-Ishq", filePath: "song/1.mp3", coverPath: 'covers/1.jpg',
+        songName: "Salam-e-Ishq", filePath: "song/1.mp3", coverPath: 'covers/1.jpg',
+        songName: "Salam-e-Ishq", filePath: "song/1.mp3", coverPath: 'covers/1.jpg',
+        songName: "Salam-e-Ishq", filePath: "song/1.mp3", coverPath: 'covers/1.jpg',
+        songName: "Salam-e-Ishq", filePath: "song/1.mp3", coverPath: 'covers/1.jpg'
+    }
+]
+
+// audioElement.play();
+
+// Handle play/pause click
+masterPlay.addEventListener('click', ()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove("fa-circle-play");
+        masterPlay.classList.add("fa-circle-pause");
+        gif.style.opacity = 1;
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove("fa-circle-pause");
+        masterPlay.classList.add("fa-circle-play");
+        gif.style.opacity = 0;
+    }
+})
+
+// Listen to events - Timeupdate is the in built function which updates the time
+    audioElement.addEventListener('timeupdate', ()=>{
+    console.log('timeupdate')
+
+    // update seekbar
+    progress = parseInt((audioElement.currentTime/audioElement.duration) * 100);
+    console.log(progress);
+    myProgressBar.value = progress;
+})
